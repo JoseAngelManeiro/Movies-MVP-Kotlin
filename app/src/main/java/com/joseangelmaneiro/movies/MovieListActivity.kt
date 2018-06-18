8ed3b9178a2fbb03a4ce95b1d760aecd716fbd5b
Jose Angel Maneiro <angel.maneiro@grupohasten.com>
2018-06-18 13:22:59 +0200

    Designed movie list screen

--- app/src/main/java/com/joseangelmaneiro/movies/MovieListActivity.kt
+++ app/src/main/java/com/joseangelmaneiro/movies/MovieListActivity.kt
@@ -3,7 +3,7 @@ package com.joseangelmaneiro.movies
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MovieListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
