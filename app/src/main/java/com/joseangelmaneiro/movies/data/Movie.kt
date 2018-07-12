8e810a798ae9a0f47c09ffb4315144f54a5bfbbd
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2018-07-12 19:17:44 +0200

    Created utility class TestUtils

--- app/src/main/java/com/joseangelmaneiro/movies/data/Movie.kt
+++ app/src/main/java/com/joseangelmaneiro/movies/data/Movie.kt
@@ -13,7 +13,7 @@ data class Movie(
         @SerializedName("poster_path") val posterPath: String,
         @SerializedName("original_language") val originalLanguage: String,
         @SerializedName("original_title") val originalTitle: String,
-        @SerializedName("genre_ids") val genreIds: List<Int>,
+        @SerializedName("genre_ids") val genreIds: List<Int>?,
         @SerializedName("backdrop_path") val backdropPath: String,
         @SerializedName("adult") val adult: Boolean,
         @SerializedName("overview") val overview: String,
