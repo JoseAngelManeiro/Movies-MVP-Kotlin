f917f1159eb19dee5580256a8aeab77af76cf796
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2018-07-24 19:34:32 +0200

    Created DetailMovieActivity

---
+++ app/src/main/java/com/joseangelmaneiro/movies/ui/detail/DetailMovieActivity.kt
@@ -0,0 +1,14 @@
+package com.joseangelmaneiro.movies.ui.detail
+
+import android.os.Bundle
+import com.joseangelmaneiro.movies.R
+import com.joseangelmaneiro.movies.ui.BaseActivity
+
+
+class DetailMovieActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_movie)
+    }
+}
