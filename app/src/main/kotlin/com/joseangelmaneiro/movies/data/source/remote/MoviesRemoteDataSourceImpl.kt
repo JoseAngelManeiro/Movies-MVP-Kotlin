d530426f5f6a9d0e2fbf8adb49fafcbabc202d1e
J. Angel Maneiro <joseangelmaneiro@yahoo.es>
2018-09-09 09:23:39 +0200

    Captured exceptions in remote call

--- app/src/main/kotlin/com/joseangelmaneiro/movies/data/source/remote/MoviesRemoteDataSourceImpl.kt
+++ app/src/main/kotlin/com/joseangelmaneiro/movies/data/source/remote/MoviesRemoteDataSourceImpl.kt
@@ -3,6 +3,8 @@ package com.joseangelmaneiro.movies.data.source.remote
 import com.joseangelmaneiro.movies.domain.Handler
 import com.joseangelmaneiro.movies.data.entity.MovieEntity
 import com.joseangelmaneiro.movies.data.entity.PageEntity
+import com.joseangelmaneiro.movies.data.exception.NetworkConnectionException
+import com.joseangelmaneiro.movies.data.exception.ServiceException
 import com.joseangelmaneiro.movies.data.source.remote.net.MovieService
 import retrofit2.Call
 import retrofit2.Callback
@@ -27,17 +29,16 @@ class MoviesRemoteDataSourceImpl private constructor(
 
     override fun getMovies(handler: Handler<List<MovieEntity>>) {
         movieService.getMovies(API_KEY).enqueue(object : Callback<PageEntity>{
-            override fun onResponse(call: Call<PageEntity>?, response: Response<PageEntity>?) {
-                val page = response?.body()
-                if(page!=null){
-                    handler.handle(page.movies)
+            override fun onResponse(call: Call<PageEntity>?, response: Response<PageEntity>) {
+                if(response.isSuccessful){
+                    handler.handle(response.body().movies)
                 } else{
-                    handler.error(Exception())
+                    handler.error(ServiceException())
                 }
             }
 
             override fun onFailure(call: Call<PageEntity>?, t: Throwable?) {
-                handler.error(Exception())
+                handler.error(NetworkConnectionException())
             }
         })
     }
